33f10e1f17f4a455b18386f848291e74c00321b8
bhaynswo <168300616+bhaynswo@users.noreply.github.com>
2024-05-01 15:30:06 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Google\CloudFunctions\FunctionsFramework;
 use Psr\Http\Message\ServerRequestInterface;
 
-FunctionsFramework::https('run', 'run');
+FunctionsFramework::http('run', 'run');
 
 function run(ServerRequestInterface $request): string
 {
